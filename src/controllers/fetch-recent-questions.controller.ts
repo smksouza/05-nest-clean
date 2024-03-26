41a3d6c527a90398a104731fd6ad63cb66f139ba
Samuel Souza <smksouza.dev@gmail.com>
2024-03-26 17:40:33 -0300

    Fetch recent questions controller E2E

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -21,7 +21,7 @@ export class FetchRecentQuestionsController {
   constructor(private prisma: PrismaService) {}
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 1
+    const perPage = 20
 
     const questions = await this.prisma.question.findMany({
       take: perPage,
