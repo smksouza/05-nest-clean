6e53b965aac5624db6f5d4d8c93f3b2308aa6747
Samuel Souza <smksouza.dev@gmail.com>
2024-03-29 18:01:01 -0300

    Implementando Prisma QuestionsRepository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,7 @@
+import { Question as PrismaQuestion, Prisma } from '@prisma/client'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
-import { Question as PrismaQuestion } from '@prisma/client'
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
     return Question.create(
@@ -18,5 +18,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updateAt,
+    }
   }
 }
