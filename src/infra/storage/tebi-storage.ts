f40dcb992931d2d6ac7106a45c132158fd3b5c4b
Samuel Souza <smksouza.dev@gmail.com>
2024-04-08 19:20:58 -0300

    Tebi/AWS api for upload file is implemented

---
+++ src/infra/storage/tebi-storage.ts
@@ -0,0 +1,48 @@
+import {
+  UploadParams,
+  Uploader,
+} from '@/domain/forum/application/storage/uploader'
+
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { EnvService } from '../env/env.service'
+import { randomUUID } from 'node:crypto'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class TebiStorage implements Uploader {
+  private client: S3Client
+
+  constructor(private envService: EnvService) {
+    this.client = new S3Client({
+      endpoint: 'https://s3.tebi.io',
+      credentials: {
+        accessKeyId: envService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: envService.get('AWS_SECRET_ACCESS_KEY_ID'),
+      },
+      region: 'global',
+    })
+  }
+
+  async upload({
+    fileName,
+    fileType,
+    body,
+  }: UploadParams): Promise<{ url: string }> {
+    const uploadId = randomUUID()
+
+    const uniqueFileName = `${uploadId}-${fileName}`
+
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: this.envService.get('AWS_BUCKET_NAME'),
+        Key: uniqueFileName,
+        ContentType: fileType,
+        Body: body,
+      }),
+    )
+
+    return {
+      url: uniqueFileName,
+    }
+  }
+}
